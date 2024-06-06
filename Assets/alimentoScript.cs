1eafdb48a6a8021e16cc92e601799de0ed74c31b
FedericoGaubeca <47962250@est.ort.edu.ar>
2024-06-06 10:41:23 -0300

    Hunger System

--- Assets/alimentoScript.cs
+++ Assets/alimentoScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class alimentoScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int valorAlimenticio;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
